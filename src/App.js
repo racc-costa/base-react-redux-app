71da42c7afbf657929407a0aa6b62d03edff995d
Ricardo de Aguiar Cavalcanti Costa (CTS) <Ricardo.Costa@fornecedores.sicoob.com.br>
2018-05-23 14:46:39 -0300

    Updated.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+
+import Counter from './containers/Counter/Counter';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-		<div>
-			<h1>Hello React!</h1>
-		</div>
+      <div className="App">
+       <Counter />
+      </div>
     );
   }
 }
