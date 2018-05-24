835c5396e65080851ee1ae9c76a086be4a1fbd89
Ricardo Costa <racc.costa@gmail.com>
2018-05-24 17:03:22 -0300

    Adding axios.

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actions';
+import axios from 'axios';
 
 const initialState = {
     counter: 0
@@ -8,6 +9,15 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.LOGIN:
             console.log('LOGIN was triggered.')
+
+            axios.get('http://www.mocky.io/v2/5b0718412f00006628c6200e')
+                .then(response => {
+                    console.log(response.data)
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+
             const newState = Object.assign({}, state);
             newState.counter = state.counter + 1;
             return newState;
